8aa55b285dcba07ec27a795dd3b78450ddeb1735
Tim-Raphael <mail@tim-raphael.dev>
2025-07-16 09:58:33 +0200

    fix(nmtui): switch to stabel

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Ahh yes, a flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     nix-colors.url = "github:misterio77/nix-colors";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };

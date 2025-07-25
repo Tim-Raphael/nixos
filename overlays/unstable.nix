67eb86e88cc0bf62ae6851d058e4c922730e27f5
Tim-Raphael <mail@tim-raphael.dev>
2025-07-25 12:42:12 +0200

    feat(overlays): add unstable

---
+++ overlays/unstable.nix
@@ -0,0 +1,11 @@
+{ inputs }:
+
+final: prev: {
+  unstable = import inputs.nixpkgs-unstable {
+    system = final.system;
+    config = {
+      allowUnfree = true;
+      allowUnsupportedSystem = false;
+    };
+  };
+}

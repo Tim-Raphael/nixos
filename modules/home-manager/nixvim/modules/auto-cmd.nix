98143ce54793e61c7a88c6694d580cb2a205bb34
Tim-Raphael <mail@tim-raphael.dev>
2025-07-26 18:49:12 +0200

    feat(nixvim): cd into cwd when closing nvim

---
+++ modules/home-manager/nixvim/modules/auto-cmd.nix
@@ -0,0 +1,34 @@
+{ ... }:
+
+{
+  programs.nixvim = {
+    # Auto groups for organizing autocommands
+    autoGroups = {
+      directory_sync = {
+        clear = true;
+      };
+    };
+
+    # Autocommand to write current directory on exit
+    autoCmd = [
+      {
+        event = [ "VimLeave" ];
+        group = "directory_sync";
+        desc = "Write current directory to file for shell sync";
+        callback = {
+          __raw = ''
+            function()
+              local cwd = vim.fn.getcwd()
+              local home = vim.fn.expand("~")
+              local file = io.open(home .. "/.nvim_last_dir", "w")
+              if file then
+                file:write(cwd)
+                file:close()
+              end
+            end
+          '';
+        };
+      }
+    ];
+  };
+}

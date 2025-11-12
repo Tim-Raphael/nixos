a077bb02f086c3313fd41c89db38f84ca56df664
Tim-Raphael <mail@tim-raphael.dev>
2025-11-12 23:27:13 +0100

    refactor: remove gtk theme

--- modules/home-manager/theme.nix
+++ modules/home-manager/theme.nix
@@ -21,21 +21,6 @@ let
   #colorScheme = inputs.nix-colors.colorSchemes.everforest;
   #colorScheme = inputs.nix-colors.colorSchemes.nord;
 
-  theme = {
-    name = "Gruvbox-Green-Dark";
-    pkg = pkgs.gruvbox-gtk-theme.override {
-      colorVariants = [ "dark" ];
-      sizeVariants = [ "standard" ];
-      themeVariants = [ "green" ];
-      tweakVariants = [ "outline" ];
-      iconVariants = [ "Dark" ];
-    };
-  };
-
-  icons = {
-    name = "oomox-gruvbox-dark";
-    pkg = pkgs.gruvbox-dark-icons-gtk;
-  };
 in
 {
   imports = [
@@ -55,55 +40,12 @@ in
       font.pkg
     ];
 
-    home.file = {
-      ".themes/${theme.name}" = {
-        source = "${theme.pkg}/share/themes/${theme.name}";
-      };
-
-      ".icons/${icons.name}" = {
-        source = "${icons.pkg}/share/icons/${icons.name}";
-      };
-
-      ".gtkrc-2.0" = {
-        text = ''
-          gtk-theme-name="${theme.name}"
-          gtk-icon-theme-name="${icons.name}"
-          gtk-font-name="${font.name} 10"
-        '';
-      };
-
-      ".config/gtk-3.0/settings.ini" = {
-        text = ''
-          [Settings]
-          gtk-theme-name=${theme.name}
-          gtk-icon-theme-name=${icons.name}
-          gtk-font-name=${font.name} 10
-        '';
-      };
-
-      ".config/gtk-4.0/settings.ini" = {
-        text = ''
-          [Settings]
-          gtk-theme-name=${theme.name}
-          gtk-icon-theme-name=${icons.name}
-          gtk-font-name=${font.name} 10
-        '';
-      };
-
-      ".profile" = {
-        text = "export GTK_THEME=${theme.name}";
-      };
-    };
-
     home.pointerCursor = {
       enable = true;
-
       gtk.enable = true;
       sway.enable = true;
       x11.enable = true;
-
       size = 16;
-
       name = "Adwaita";
       package = pkgs.adwaita-icon-theme;
     };

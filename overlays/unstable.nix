879e851e85814ae86de22c267519ffb50edb8a20
Tim-Raphael <mail@tim-raphael.dev>
2025-12-12 11:11:00 +0100

    fix: use stdenv.hostPlatform.system in unstable overlay

--- overlays/unstable.nix
+++ overlays/unstable.nix
@@ -2,7 +2,7 @@
 
 final: prev: {
   unstable = import inputs.nixpkgs-unstable {
-    system = final.system;
+    system = final.stdenv.hostPlatform.system;
     config = {
       allowUnfree = true;
       allowUnsupportedSystem = false;

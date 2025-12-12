0a99d0bbe3d9486eb440dc8d31b597df6f3a38c5
Tim-Raphael <mail@tim-raphael.dev>
2025-12-12 11:06:10 +0100

    feat(nixvim): configure blink-cmp

--- modules/home-manager/nixvim/modules/blink-cmp.nix
+++ modules/home-manager/nixvim/modules/blink-cmp.nix
@@ -2,11 +2,33 @@
 
 {
   programs.nixvim.plugins = {
+    blink-cmp-dictionary.enable = true;
+    blink-cmp-spell.enable = true;
+    # TODO: Lazylaod when opening a .tex file.
+    blink-cmp-latex.enable = true;
+
     blink-cmp = {
       enable = true;
       settings = {
-        completion.list.selection.preselect = false;
         keymap.preset = "enter";
+        completion = {
+          list.selection = {
+            preselect = false;
+            auto_insert = false;
+          };
+        };
+        cmdline = {
+          completion = {
+            menu = {
+              auto_show = true;
+            };
+            list.selection = {
+              preselect = false;
+              auto_insert = true;
+            };
+          };
+          keymap.preset = "default";
+        };
       };
     };
   };

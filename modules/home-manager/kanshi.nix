834def25533235a8668065e14b3e54b3311aeb97
Tim-Raphael <mail@tim-raphael.dev>
2026-01-22 19:17:38 +0100

    feat(kanshi): setup new home monitor

--- modules/home-manager/kanshi.nix
+++ modules/home-manager/kanshi.nix
@@ -47,16 +47,9 @@
         profile.name = "home";
         profile.outputs = [
           {
-            criteria = "HDMI-A-1";
-            status = "enable";
-            mode = "1920x1080@74.973000Hz";
-            position = "0,0";
-            scale = 1.0;
-          }
-          {
             criteria = "DP-1";
             status = "enable";
-            mode = "1920x1080@143.854996Hz";
+            mode = "3840x2160@120.000000Hz";
             position = "1920,0";
             scale = 1.0;
           }

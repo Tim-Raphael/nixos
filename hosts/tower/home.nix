d7fc4f5910b84d0769f11275fc3a5c95e7804521
Tim-Raphael <mail@tim-raphael.dev>
2026-02-05 22:19:32 +0100

    feat(tower): add custom user dirs

--- hosts/tower/home.nix
+++ hosts/tower/home.nix
@@ -25,6 +25,7 @@
     ../../modules/home-manager/crypt.nix
     ../../modules/home-manager/kanshi.nix
     ../../modules/home-manager/i3status.nix
+    ../../modules/home-manager/user-dirs.nix
   ];
 
   i3status = {

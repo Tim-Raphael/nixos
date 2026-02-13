819e5613a1aaad3440c36040cd522ddb0bf56a4f
Tim-Raphael <mail@tim-raphael.dev>
2026-02-13 11:49:19 +0100

    feat(nixvim): unmap C-h and C-l in oil

--- modules/home-manager/nixvim/modules/oil.nix
+++ modules/home-manager/nixvim/modules/oil.nix
@@ -24,6 +24,10 @@
           view_options = {
             show_hidden = true;
           };
+          keymaps = {
+            "<C-l>" = false;
+            "<C-h>" = false;
+          };
         };
       };
     };

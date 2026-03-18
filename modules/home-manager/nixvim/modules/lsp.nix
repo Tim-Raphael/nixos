5bd39954aa56e11c6d479b02a114d24bb638ba5d
Tim-Raphael <mail@tim-raphael.dev>
2026-03-18 13:29:45 +0100

    fix(nixvim): use stable latest rust-analzyer

--- modules/home-manager/nixvim/modules/lsp.nix
+++ modules/home-manager/nixvim/modules/lsp.nix
@@ -18,6 +18,13 @@
           };
           rust_analyzer = {
             enable = true;
+            package = pkgs.rust-bin.stable.latest.minimal.override {
+              extensions = [
+                "rust-src"
+                "rust-analyzer"
+              ];
+              targets = [ "x86_64-unknown-linux-gnu" ];
+            };
             # Provided by default rust toolchain profile
             installCargo = false;
             installRustc = false;

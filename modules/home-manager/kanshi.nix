71171886f0271b5aa3f69daa23c3c1234b34eba7
Tim-Raphael <mail@tim-raphael.dev>
2026-04-10 09:44:04 +0200

    chore(deps): update opencode

--- modules/home-manager/kanshi.nix
+++ modules/home-manager/kanshi.nix
@@ -27,7 +27,7 @@
             scale = 1.0;
           }
           {
-            criteria = "DP-6";
+            criteria = "DP-5";
             status = "enable";
             mode = "3840x2160@120.000000";
             position = "0,0";
@@ -59,7 +59,7 @@
             scale = 1.0;
           }
           {
-            criteria = "DP-2";
+            criteria = "DP-1";
             status = "enable";
             mode = "2560x1440@59.951000Hz";
             position = "0,0";
@@ -72,15 +72,15 @@
         profile.name = "hackathon";
         profile.outputs = [
           {
-            criteria = "HDMI-A-1";
+            criteria = "DP-2";
             status = "enable";
-            mode = "2560x1440@59.951000Hz";
+            mode = "3440x1440@49.987000Hz";
             position = "0,0";
             scale = 1.0;
           }
           {
             criteria = "eDP-1";
-            status = "enable";
+            status = "disable";
             position = "2560,0";
             scale = 1.0;
           }

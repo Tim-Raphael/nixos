cc35c41dd3547b32f8b3ae13cab2474516a5f0b1
Tim-Raphael <mail@tim-raphael.dev>
2026-05-22 16:22:50 +0200

    feat(kanshi): enable auto configuration

--- modules/home-manager/kanshi.nix
+++ modules/home-manager/kanshi.nix
@@ -1,5 +1,8 @@
 { ... }:
 
+let
+  best_mode_path = "kanshi/best_mode.sh";
+in
 {
   services.kanshi = {
     enable = true;
@@ -15,77 +18,66 @@
             scale = 1.0;
           }
         ];
+        profile.exec = [ "~/.config/${best_mode_path}" ];
       }
 
       {
-        profile.name = "home-docked";
+        profile.name = "docked";
         profile.outputs = [
           {
             criteria = "eDP-1";
             status = "disable";
-            position = "0,0";
-            scale = 1.0;
           }
           {
-            criteria = "DP-5";
+            criteria = "*";
             status = "enable";
-            mode = "3840x2160@120.000000";
             position = "0,0";
             scale = 1.0;
           }
         ];
-      }
-
-      {
-        profile.name = "home";
-        profile.outputs = [
-          {
-            criteria = "DP-3";
-            status = "enable";
-            mode = "3840x2160@120.000000Hz";
-            position = "0,0";
-            scale = 1.0;
-          }
-        ];
-      }
-
-      {
-        profile.name = "work";
-        profile.outputs = [
-          {
-            criteria = "eDP-1";
-            status = "disable";
-            position = "0,0";
-            scale = 1.0;
-          }
-          {
-            criteria = "DP-1";
-            status = "enable";
-            mode = "2560x1440@59.951000Hz";
-            position = "0,0";
-            scale = 1.0;
-          }
-        ];
-      }
-
-      {
-        profile.name = "hackathon";
-        profile.outputs = [
-          {
-            criteria = "DP-2";
-            status = "enable";
-            mode = "3440x1440@49.987000Hz";
-            position = "0,0";
-            scale = 1.0;
-          }
-          {
-            criteria = "eDP-1";
-            status = "disable";
-            position = "2560,0";
-            scale = 1.0;
-          }
-        ];
+        profile.exec = [ "~/.config/${best_mode_path}" ];
       }
     ];
+  };
+
+  xdg.configFile."${best_mode_path}" = {
+    executable = true;
+    text = ''
+      #!/bin/sh
+      # wlr-randr output looks like:
+      #   eDP-1 "..." 
+      #     ...
+      #     Modes:
+      #       1920x1200 px, 59.950001 Hz (preferred, current)
+      #       1920x1200 px, 47.959999 Hz
+      #     ...
+
+      wlr-randr | awk '
+        /^[^ \t]/ {
+          if (output != "" && best_res != "") {
+            print output " " best_res "@" best_hz "Hz"
+          }
+          output = $1
+          best_res = ""; best_pixels = 0; best_hz = 0
+        }
+        /^[ \t]+[0-9]+x[0-9]+ px,/ {
+          split($1, res, "x")
+          pixels = res[1] * res[2]
+          hz = $3 + 0
+          if (pixels > best_pixels || (pixels == best_pixels && hz > best_hz)) {
+            best_pixels = pixels
+            best_hz = hz
+            best_res = $1
+          }
+        }
+        END {
+          if (output != "" && best_res != "") {
+            print output " " best_res "@" best_hz "Hz"
+          }
+        }
+      ' | while read -r output mode; do
+        wlr-randr --output "$output" --mode "$mode"
+      done
+    '';
   };
 }

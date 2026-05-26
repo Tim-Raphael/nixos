7c2e149821821026ad16a23a0231219413119169
Tim-Raphael <mail@tim-raphael.dev>
2026-05-26 11:08:06 +0200

    feat(keyboard): align layouts

--- modules/system/keyboard.nix
+++ modules/system/keyboard.nix
@@ -14,32 +14,18 @@
       internalKeyboard = {
         devices = [
           "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-
-          "/dev/input/by-id/usb-SONiX_USB_DEVICE-event-if01"
-          "/dev/input/by-id/usb-SONiX_USB_DEVICE-event-kbd"
-
-          "/dev/input/by-id/usb-Cherry_USB_keyboard-event-if01"
-          "/dev/input/by-id/usb-Cherry_USB_keyboard-event-kbd"
-
-          "/dev/input/by-id/usb-Cherry_GmbH_CHERRY_Corded_Device-event-if01"
           "/dev/input/by-id/usb-Cherry_GmbH_CHERRY_Corded_Device-event-kbd"
-
-          "/dev/input/by-id/usb-NuPhy_NuPhy_Gem80_42003E000E5359583534332000000000-if02-event-kbd"
         ];
 
         extraDefCfg = "process-unmapped-keys yes";
 
         config = ''
           (defsrc
-            caps
-          )
-
-          (defalias
-            capsesc (tap-hold 200 200 esc caps)
+            esc caps lalt ralt lmet rmet
           )
 
           (deflayer nrml
-            @capsesc
+            caps esc lmet rmet lalt ralt
           )
         '';
       };

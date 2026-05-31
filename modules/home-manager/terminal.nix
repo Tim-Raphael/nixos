1ccd8d40206d044ae63901cd3321476414fc9f75
Tim-Raphael <mail@tim-raphael.dev>
2026-05-31 16:58:38 +0200

    feat(shell): remember last directory

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -1,7 +1,5 @@
 {
   pkgs,
-  config,
-  lib,
   ...
 }:
 
@@ -24,7 +22,8 @@
 
     interactiveShellInit = ''
       set fish_greeting
-      set PATH $HOME/.local/bin $PATH # for some util scripts
+      set PATH $HOME/.local/bin $PATH # For some util scripts
+      set PATH $HOME/.cargo/bin $PATH
       set -gx GPG_TTY (tty)
 
       # Use vim key-bindings
@@ -35,6 +34,19 @@
 
       # Init pay-respects
       pay-respects fish | source
+
+      # Remember last directory
+      function __remember_pwd --on-variable PWD
+          echo $PWD > ~/.cache/fish_last_pwd
+      end
+
+      # Load last directory on startup
+      if status is-interactive
+          set -l last_pwd ~/.cache/fish_last_pwd
+          if test -f $last_pwd
+              cd (cat $last_pwd)
+          end
+      end
     '';
   };
 }

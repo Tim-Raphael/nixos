f7417fa37020cd206619d2c4171dc9e5e8864c1f
Tim-Raphael <mail@tim-raphael.dev>
2026-06-01 19:41:12 +0200

    feat(browser): adjust firefox styles

--- modules/home-manager/browser/firefox.nix
+++ modules/home-manager/browser/firefox.nix
@@ -2,6 +2,7 @@
 
 {
   stylix.targets.firefox.profileNames = [ "main" ];
+
   programs.firefox = {
     enable = true;
     profiles.main = {
@@ -15,6 +16,35 @@
           darkreader
         ];
       };
+      settings = {
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      };
+      userChrome = ''
+        #tabbrowser-tabs {
+          &[orient="horizontal"] {
+           --tab-max-width: none !important;
+          }
+          .tab-label-container {
+            height: unset !important;
+          }
+        }
+      '';
+    };
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
     };
   };
 }

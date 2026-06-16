2a8266240f73bfd2538302a050a026c8f7305bde
Tim-Raphael <mail@tim-raphael.dev>
2026-06-16 15:08:13 +0200

    feat(virtualisation): switch to podman

--- modules/system/virtualisation.nix
+++ modules/system/virtualisation.nix
@@ -1,13 +1,18 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   virtualisation = {
-    docker.enable = true;
     podman = {
       enable = true;
       defaultNetwork.settings.dns_enabled = true;
+      dockerCompat = true;
+      dockerSocket.enable = true;
     };
     containers.enable = true;
   };
-
+  environment.systemPackages = with pkgs; [
+    docker-compose
+    podman-compose
+    podman-tui
+  ];
 }

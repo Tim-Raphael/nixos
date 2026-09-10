1d8ed60cf678588682bc353e9bda343102e3389a
Tim-Raphael <mail@tim-raphael.dev>
2026-09-10 09:10:08 +0200

    Revert "Fix add kagi as default search engine"
    This reverts commit f3c01176d2d7a1d8bf635f138fc4d68f325f1775.

--- modules/home-manager/browser/firefox.nix
+++ modules/home-manager/browser/firefox.nix
@@ -11,20 +11,18 @@
     # than upstream ships them, so track unstable.
     package = pkgs.unstable.firefox;
     configPath = ".mozilla/firefox";
-    policies = {
-      SearchEngines = {
-        Add = [
-          {
-            Name = "Kagi";
-            URLTemplate = "https://kagi.com/search?q={searchTerms}";
-            Method = "GET";
-            Alias = "@kagi";
-          }
-        ];
-        Default = "Kagi";
-      };
-    };
     profiles.main = {
+      search = {
+        force = true;
+        default = "kagi";
+        engines = {
+          kagi = {
+            name = "Kagi";
+            urls = [ { template = "https://kagi.com/search?q={searchTerms}"; } ];
+            definedAliases = [ "@kagi" ];
+          };
+        };
+      };
       extensions = {
         packages = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin

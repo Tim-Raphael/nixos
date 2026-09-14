c7e80858a4f010b7b24fb8fd2487c41a9e256908
Tim-Raphael <mail@tim-raphael.dev>
2026-09-14 18:48:06 +0200

    Set brave as default browser

--- modules/home-manager/browser/brave.nix
+++ modules/home-manager/browser/brave.nix
@@ -2,4 +2,20 @@
 
 {
   programs.brave.enable = true;
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/x-extension-htm" = "brave-browser.desktop";
+      "application/x-extension-html" = "brave-browser.desktop";
+      "application/x-extension-shtml" = "brave-browser.desktop";
+      "application/x-extension-xht" = "brave-browser.desktop";
+      "application/x-extension-xhtml" = "brave-browser.desktop";
+      "application/xhtml+xml" = "brave-browser.desktop";
+      "text/html" = "brave-browser.desktop";
+      "x-scheme-handler/chrome" = "brave-browser.desktop";
+      "x-scheme-handler/http" = "brave-browser.desktop";
+      "x-scheme-handler/https" = "brave-browser.desktop";
+    };
+  };
 }
